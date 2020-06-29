c6ab9aee1c88713bff8a27634b0735823adf484b
Micheal Hansen <micheal.hansen@gmail.com>
2020-06-29 13:21:55 -0700

    add initial user interface logic
    Co-authored-by: Tristan Emmerson <tristan@stickerslug.com>

--- src/main.js
+++ src/main.js
@@ -2,7 +2,21 @@ import $ from 'jQuery.js';
 import 'bootstrap.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+import { Pet } from './pet.js';
+
+function attachNewPetListener() {
+  $('#addPet').click(function(event) {
+    event.preventDefault();
+    const petName = $('#petName').val();
+    if (!petName) {
+      alert('Enter a name for your new pet');
+      return;
+    }
+    let newPet = new Pet(petName);
+    alert(newPet.name);
+  });
+}
 
 $(document).ready(function() {
-  
-})
+  attachNewPetListener();
+});
